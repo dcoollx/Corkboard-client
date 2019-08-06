378fc9e0319d8832edd2accbd001582eba45b56c
David Queen <dsqjr@msn.com>
2019-08-06 12:42:40 -0400

    wired up to server

--- src/App.js
+++ src/App.js
@@ -5,26 +5,40 @@ import Main from './components/main/main';
 import Login from './components/login/login';
 import NoticePage from './components/main/announcements/NoticePage/noticePage';
 import {testNotices} from './test/demoData';
+import Api from './services/api.service';
 
 class App extends React.Component {
   state = {
     orgName:null,
-    announcements:[...testNotices(5)],
+    notices:[...testNotices(5)],
     isSignedIn: false,
     hasError:false,
+    err:null
   };
+  componentDidMount(){
+    let options ={
+      method:'GET',
+      headers: new Headers({'content-type':'application/json'})
+    };
+    Api.doFetch('1/corkboard',options)
+      .then(notices=>this.setState({notices:notices.notices}))
+      .catch(err=>{
+        this.setState({hasError:true,err})
+        console.log('fetch encountered an error', err);
+      });
+  }
   render(){
-    console.log(this.state.announcements)
+    console.log(this.state.notices)
   return (
     <div className="App">
       <Route exact path="/" render={(props)=>{
         return(
-        <Main {...props} announcements={this.state.announcements}/>
+        <Main {...props} announcements={this.state.notices}/>
         );
         }}/>
       <Route exact path="/login" component={Login}/>
       <Route exact path="/notice/:id" render={(props)=>{
-        return(<NoticePage {...props} notice={this.state.announcements.find((notice)=>props.match.params.id === String(notice.id))}/>);
+        return(<NoticePage {...props} notice={this.state.notices.find((notice)=>props.match.params.id === String(notice.id))}/>);
       }}/>
     </div>
   );
---
+++ src/services/api.service.js
@@ -0,0 +1,12 @@
+export default class Api{
+  static url = 'http://localhost:8000/';
+  static Api_Key = '';
+  static doFetch(endpoint,options){
+    return fetch(this.url + endpoint).then(res=>{
+      if(!res.ok)
+        return Promise.reject(new Error(res.json()));
+      else
+        return res.json();
+    })
+  }
+}
