010b60cb449a9ad8d05e73656e93ed7cbb4d2b84
David Queen <dsqjr@msn.com>
2019-08-07 17:01:52 -0400

    added auth

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,11 @@
+import tokenService from "./token.service";
+
 export default class Api{
   static url = 'http://localhost:8000/';
   static Api_Key = '';
-  static doFetch(endpoint,options=null){
+  static doFetch(endpoint,options= {Method:'GET', headers:new Headers({'content-type':'application/json'})}){
+    if(tokenService.hasAuthToken())
+      options.headers.set('Authorization','bearer ' + tokenService.getAuthToken());
     return fetch(this.url + endpoint,options).then(res=>{
       if(!res.ok)
         return Promise.reject(new Error(res.json()));
